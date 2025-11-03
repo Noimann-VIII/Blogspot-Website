3d00c6a4fc99ea71a30825a16fb3e7259225387d
kayisqvl <kristelleanneesquivel@gmail.com>
2025-11-03 17:08:20 +0800

    8.3

--- blogspot/src/pages/NotFound/NotFound.jsx
+++ blogspot/src/pages/NotFound/NotFound.jsx
@@ -4,8 +4,7 @@ import bgImage from "../assets/404bg.jpg";
 function NotFound() {
   return (
     <>
-
-    
+   
       <h1>404</h1>
       <p>Oops! The page you're looking for doesn't exist or has been moved.</p>
     </>
