9c650c0bc1af74fabc138110728641b9aaf745d4
roy-hub01 <janroy.cruz@email.lcup.edu.ph>
2025-11-03 19:01:08 +0800

    finished about

---
+++ blogspot/src/components/MemberCard/MemberCard.jsx
@@ -0,0 +1,20 @@
+import "./member-card.css"
+
+function MemberCard(m){
+    return(
+        <>
+            <div className="member-card">
+            <div>
+            <img src={m.imageURL}alt={m.name} />
+        </div>
+            <div>
+            <h3>{m.name}</h3>
+            <p>{m.age} years old</p>
+            </div>
+ 
+        </div>
+        </>
+    )
+}
+
+export default MemberCard
