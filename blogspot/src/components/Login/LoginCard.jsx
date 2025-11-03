2ee4e60867385b3924b304c25c583912c1f3d57b
noimann <the.noimann.8@gmail.com>
2025-11-03 18:13:15 +0800

    finished login page

--- blogspot/src/components/Login/LoginCard.jsx
+++ blogspot/src/components/Login/LoginCard.jsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
 import "./login-card.css";
 import { NavLink } from "react-router";
-// import { signInWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../../firebase-config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase-config";
 
 function LoginCard() {
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  // function handleLogin() 
-  // {
-  //   signInWithEmailAndPassword(auth, email, password)
-  //   .then(()=>{window.location.href="/home";
-  //   }).catch((error)=>{
-  //     alert(error.message);
-  //   })
-  // }
+  function handleLogin(e) 
+  {
+    e?.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href="/home";
+    }).catch((error) => {
+      alert(error.message);
+    })
+  }
 
   return (
     <>
     <div className="login-card">
         <h1>Login</h1>
         <form>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Login</button>
+            <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Email" />
+            <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
         </form>
         <NavLink to="/register">Don't have an account? Register here.</NavLink>
     </div>
